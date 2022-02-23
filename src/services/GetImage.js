fe1f5332481ff6b10495f5ab791f93a1cbdc25c4
IsaGarabana <isinhagb@gmail.com>
2022-02-23 19:08:54 +0100

    change text on undefined image

--- src/services/GetImage.js
+++ src/services/GetImage.js
@@ -1,6 +1,6 @@
 function getImage(name, image) {
 	return image === ""
-		? `https://dummyimage.com/200x300/432f4d/ffa500.png&text=${name},+invisible.`
+		? `https://dummyimage.com/200x300/432f4d/ffa500.png&text=${name},+is now invisible.`
 		: image;
 }
 
